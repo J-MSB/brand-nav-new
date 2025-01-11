bbfc320d460595b72dcc04f4afbd5f6232d64ab1
maheshbabu <jakkulamaheshbabu8@gmail.com>
2025-01-11 21:57:48 +0530

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+  basePath: '/brand-nav-new',  // Replace with your GitHub repository name
+}
